7864e5a1ce4b487af97893615929ac7d138852d0
makowiec <ptosiek@poczta.fm>
2015-07-03 12:43:08 +0200

    - dodano listowanie plików znajnujące się w określonej lokalizacji;

--- src/list.cpp
+++ src/list.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
 
 using namespace std;
 
@@ -52,39 +55,39 @@ string jednostka_pliku (double rozmiar) //okreœlenie jednostki pliku
 
 	if (rozmiar < 1024)
 		{
-		jednostka_fun = " B";
+		jednostka_fun = " B ; ";
 		}
 		else
 		{
 			if (rozmiar < 1048576)
 			{
-			jednostka_fun = " kB";
+			jednostka_fun = " kB ; ";
 			}
 			else
 			{
 				if (rozmiar < 1073741824)
 				{
-				jednostka_fun = " MB";
+				jednostka_fun = " MB ; ";
 				}
 				else
 				{
 					if (rozmiar < 1099511627776)
 					{
-					jednostka_fun = " GB";
+					jednostka_fun = " GB ; ";
 					}
 					else
 					{
-					jednostka_fun = " Error";
+					jednostka_fun = " Error ; ";
 		}	}	}	}
 
 	return jednostka_fun;
 }
 
+void listDir(const char * dirn, fstream &file);
+
 int main()
 {
-	double rozmiar; //rozmiar pliku w bajtach
-	double wynik; // rozmiar pliku w "najwiêkszych jednostkach"
-	string jednostka; //do zapisu jednostki
+
 	string kod; //kod - potrzebny do œcie¿ki
 	string rok; //rok - potrzebny do œcie¿ki
 	string miesiac; //miesi¹c - potrzebny do œcie¿ki
@@ -101,16 +104,6 @@ int main()
 
 	//cout << sciezka << endl;
 
-	fstream plik("plik.txt"); //strumien na pliku
-	plik.seekg(0, ios::end); //przesuniêcie wskaznika na koniec
-	//cout << plik.tellg(); // pozycja wskaŸnika na koñcu pliku
-
-	rozmiar = plik.tellg();
-	plik.close(); //zamykanie pliku
-
-	wynik = rozmiar_pliku (rozmiar);
-	jednostka = jednostka_pliku (rozmiar);
-
 	fstream file;
 	// otwarcie pliku
 	file.open( "wynik.txt", ios::app | ios::in | ios::out );
@@ -118,23 +111,72 @@ int main()
 	if( file.good() == true )
 	{
 // komentarz sprawdzajacy
-	cout << "Uzyskano dostep do pliku!" << endl;
+cout << "Uzyskano dostep do pliku!" << endl;
 
-	//zapisanie strumienia do pliku
-	file << setprecision(3) << wynik << jednostka << endl;
-
-// komentarz sprawdzajacy
-	cout << "Dane zapisane do pliku" << endl;
+	listDir("C:\\baza", file);
+    }
 
 	//obs³uba b³êdu - brak dostêpu do pliku
-	}
 	else
+	{
 // komentarz sprawdzajacy
-	cout << "Dostep do pliku zostal zabroniony!" << endl;
-
+cout << "Dostep do pliku zostal zabroniony!" << endl;
+	}
 	// zamykanie pliku
 	file.close();
 // komentarz sprawdzajacy
-	cout << "Plik zostal zamkniety" << endl;
+cout << "Plik zostal zamkniety" << endl;
+
 	return 0;
+}
+
+void listDir(const char * dirn, fstream &file)
+{
+
+	double rozmiar; //rozmiar pliku w bajtach
+	double wynik; // rozmiar pliku w "najwiêkszych jednostkach"
+	string jednostka; //do zapisu jednostki
+
+	char dirnPath[1024];
+    sprintf(dirnPath, "%s\\*", dirn);
+
+    WIN32_FIND_DATA f;
+    HANDLE h = FindFirstFile(dirnPath, &f);
+
+    if (h == INVALID_HANDLE_VALUE) { return; }
+
+    do
+    {
+        const char * name = f.cFileName;
+
+        if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0) { continue; }
+
+        char filePath[1024];
+        sprintf(filePath, "%s%s%s", dirn, "\\", name);
+
+        //file << filePath << endl;
+
+        fstream plik(filePath); //strumien na pliku
+        	plik.seekg(0, ios::end); //przesuniêcie wskaznika na koniec
+        	//cout << plik.tellg(); // pozycja wskaŸnika na koñcu pliku
+
+        	rozmiar = plik.tellg();
+        	plik.close(); //zamykanie pliku
+
+        	wynik = rozmiar_pliku (rozmiar);
+        	jednostka = jednostka_pliku (rozmiar);
+
+        	file << setprecision(3) << wynik << jednostka << filePath << endl;
+
+
+        // komentarz sprawdzajacy
+        	cout << "Dane zapisane do pliku" << endl;
+
+        if (f.dwFileAttributes&FILE_ATTRIBUTE_DIRECTORY)
+        {
+            listDir(filePath, file);
+        }
+
+    } while (FindNextFile(h, &f));
+    FindClose(h);
 }
